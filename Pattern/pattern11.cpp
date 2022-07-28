a2beeaed1e3d4e189a9f01a1b1116d6af18e3c4f
sahiljawale54 <20510057.dypit@dypvp.edu.in>
2022-07-28 18:12:59 +0530

    pattern 11&12

---
+++ Pattern/pattern11.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+// D
+// C D
+// B C D
+// A B C D
+int main(){
+  int n;
+  cin>>n;
+
+  int row=1;
+  while(row<=n){
+    int col=1;
+    char ch ='A'+n-row;
+    while(col<=row){
+      cout<<ch<<" ";
+      ch++;
+      col++;
+    }
+    cout<<"\n";
+    row++;
+  }
+  return 0;
+}
